58e7b70498f1b0f76524f7bd510d16e7365538fb
funsworkings <funsworkings@gmail.com>
2018-10-07 22:16:18 -0700

    Initial Unity commit (with basic physics/interaction

---
+++ canoes/Assets/scripts/Follows.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follows : MonoBehaviour {
+	public Transform target;
+	public bool isOffset;
+
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		if (target != null && isOffset)
+			CalculateOffset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOffset)
+			transform.position = target.position + offset;
+		else
+			transform.position = target.position;
+	}
+
+	void CalculateOffset(){
+		offset = (transform.position - target.position);
+	}
+}
---
+++ canoes/Assets/scripts/Person.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+	Rigidbody canoe;
+	public float forwardForce, sideForce;
+
+	void Awake(){
+		canoe = transform.parent.GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int side = 0;
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+			--side;
+		if (Input.GetKeyDown (KeyCode.RightArrow))
+			++side;
+
+		if (side != 0)
+			Paddle (side);
+	}
+
+	void Paddle(int dir){
+		canoe.AddForceAtPosition (transform.forward * forwardForce, transform.position + transform.right * dir * 2f);
+	}
+}
